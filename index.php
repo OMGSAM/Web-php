365115a98c61663763ea93f588e50e226361d1fe
ARNOLD <146892654+OMGSAM@users.noreply.github.com>
2024-12-19 19:06:09 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestion des élections</title>
+</head>
+<body>
+    <h1>Gestion des élections législatives</h1>
+    <nav>
+        <ul>
+            <li><a href="liste_candidats.php">Liste des candidats (niveau supérieur)</a></li>
+            <li><a href="circonscription_candidats.php">Candidats en tête des listes</a></li>
+            <li><a href="gestion_circonscriptions.php">Gestion des circonscriptions</a></li>
+            <li><a href="recherche_circonscriptions.php">Recherche par initiales</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
